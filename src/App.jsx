dfe537ebc755069fd1eee6ac1f05b0056d1bb225
dhruvxop19 <dhruvxop19@gmail.com>
2025-09-01 22:49:53 +0530

    fetched smthg from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { useState } from "react"
-
-//custom hook
-function useCounter() {
-  const [count, setCount] = useState(0);
-
-  function increaseCount() {
-    setCount(c => c + 1)
-  }
-
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-
-}
+import { useEffect, useState } from "react"
 
 function App() {
-  const { count, increaseCount } = useCounter();
+  const [post, setPost] = useState({})
 
-  return (<div>
-    <button onClick={increaseCount}>Increase {count}</button>
-  </div>)
+  async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const json = await response.json();
+    setPost(json);
+
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+
+  return (
+    <div>
+      {post.title}
+    </div>)
+
 }
-
 export default App
